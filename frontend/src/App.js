331f42237de31a4b0be2f06ea2d2f2f5e2f98106
Lena Vertieva <elena.viktorovna.bogatyreva@gmail.com>
2019-10-14 22:25:32 +0300

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,9 @@ import React, {useState, useEffect}  from 'react'
 import useJsonFetch from './hooks/useJsonFetch'
 
 export default function App() {
-  
   const [items] = useJsonFetch(process.env.REACT_APP_ITEMS_URL)
-  const [data, setData] = useState([])
-  const [draggedTask, setDraggedTask] = useState({})
+  const [data, setData] = useState([]) // БД
+//  const [draggedTask, setDraggedTask] = useState({})
 
   useEffect(() => {
     if(items.length > 0) {
@@ -14,11 +13,21 @@ export default function App() {
   }, [items])
 
   const handleOnDrag = (event, todo) => {
-    event.preventDefault();
-    setDraggedTask(todo)
+    event.preventDefault()
+    let newArr = data.filter(task => task.id !== todo.id)
+    console.log(newArr)
+    setData([...data, newArr])
   }
 
+  const handleOnDragOver = (event) => {
+    event.preventDefault();
+  }
 
+  const handleOnDrop = (event, todo) => {
+    event.preventDefault();
+
+
+  }
 
   return (
     <table className="table table-dark">
@@ -33,13 +42,14 @@ export default function App() {
 
       <tbody>
         {data.length > 0 && data.map(o => (
-          <tr key={o.id} draggable onDrag={(e) => handleOnDrag(e, o)}>
+          <tr key={o.id} draggable onDrag={(e) => handleOnDrag(e, o)} onDrop={e => handleOnDrop(e, o)} onDragOver={e => handleOnDragOver(e)}>
             <th scope="row">{o.npp}</th>
             <td>{o.user}</td>
             <td>{o.task}</td>
             <td>{o.date}</td>
           </tr>
         ))}
+
       </tbody>
     </table>
   );
